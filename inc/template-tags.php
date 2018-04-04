f9969f68ec5892fe0ad6df969dfce2dcf95801e3
jomurgel <dev@jomurgel.com>
2018-04-03 19:01:32 -0600

    replace text domain

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -25,12 +25,12 @@ function acorn_get_svg( $args = array() ) {
 
 	// Make sure $args are an array.
 	if ( empty( $args ) ) {
-		return __( 'Please define default parameters in the form of an array.', 'twentyseventeen' );
+		return __( 'Please define default parameters in the form of an array.', 'acorn' );
 	}
 
 	// Define an icon.
 	if ( false === array_key_exists( 'icon', $args ) ) {
-		return __( 'Please define an SVG icon filename.', 'twentyseventeen' );
+		return __( 'Please define an SVG icon filename.', 'acorn' );
 	}
 
 	// Set defaults.
